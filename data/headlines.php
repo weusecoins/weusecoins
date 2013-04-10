2feb962273dfafe5936c704e666d9d618bd46793
Stefan Thomas <justmoon@members.fsf.org>
2013-04-10 20:55:04 +0200

    Change headlines.php to be a cronjob.

--- data/headlines.php
+++ data/headlines.php
@@ -1,5 +1,6 @@
 <?
-if(is_file('headlines.html')&&filemtime('headlines.html')>time()-86400){echo file_get_contents('headlines.html');exit;}
+$filename = dirname(__FILE__) . '/headlines.html';
+if(is_file($filename)&&filemtime($filename)>time()-86400){exit;}
 $data=file_get_contents('http://bitcoinwatch.com/headlines.html');
 if($data===false||strpos($data,'<h3>')===false)exit;
 $data=substr($data,strpos($data,'<h3>'));
@@ -8,6 +9,5 @@ $data=explode('</ul>',$data);
 if(count($data)<6)exit;
 $ndata=implode('</ul>',array($data[0],$data[1],$data[2],$data[3],$data[4],$data[5])).'</ul><a class="more" href="http://bitcoinwatch.com/headlines.html">more..</a>';
 $ndata=str_replace('<h3>','<p>',str_replace('</h3>','</p>',$ndata));
-file_put_contents('headlines.html',$ndata);
-echo $ndata;
+file_put_contents($filename,$ndata);
 ?>
